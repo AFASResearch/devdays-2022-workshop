dc4b7ddfafb861b2410b9c941d0e5da322e9245d
Johan Gorter <johan-gorter@users.noreply.github.com>
2022-09-19 15:13:28 +0200

    progress

--- WorkshopLibrary/KvkOrganization.cs
+++ WorkshopLibrary/KvkOrganization.cs
@@ -4,6 +4,7 @@ public class KvkOrganization
   public string kvkNummer { get; set; }
   public string rsin { get; set; }
   public List<KvkHandelsnaam> handelsnamen { get; set; }
+  public string naam { get; set; }
 }
 
 public class KvkHandelsnaam
--- WorkshopSnippets/Kvk.cs
+++ WorkshopSnippets/Kvk.cs
@@ -1,6 +1,6 @@
 public class KvkSnippets
 {
-  public async Task FindByKvk(string kvkNummer, string apiKey, Func<string, Task> Log)
+  public async Task<bool> FindByKvk(string kvkNummer, string apiKey, Func<string, Task> Log)
   {
     using var kvkClient = new HttpClient();
     kvkClient.DefaultRequestHeaders.Add("apikey", apiKey);
@@ -10,18 +10,25 @@ public class KvkSnippets
 
     try {
       var result = await kvkClient.GetFromJsonAsync<KvkOrganization>(url);
-      await Log("Found " + string.Join(", ", result!.handelsnamen.OrderBy(h => h.volgorde).Select(h => h.naam)));
+      var name = result!.naam;
+      if (result!.handelsnamen != null) 
+      {
+        name = string.Join(", ", result!.handelsnamen!.OrderBy(h => h.volgorde).Select(h => h.naam));
+      }
+      await Log("Found " + name);
     } 
     catch (HttpRequestException e)
     {
       if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
       {
         await Log("Not Found");
+        return false;
       }
       else
       {
         throw;
       }
     }
+    return true;
   }
 }
